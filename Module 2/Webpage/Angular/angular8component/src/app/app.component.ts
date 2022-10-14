ef4321b686bea77503401c3a81b40ade9e30c698
Rusafid Ahmed <61000450+rusafidt@users.noreply.github.com>
2022-10-14 14:03:48 +0530

    Tr

---
+++ Module 2/Webpage/Angular/angular8component/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'first_project';
+  list: any[] = [];
+  add(item: String) {
+
+    this.list.push({ item });
+    console.log(this.list)
+
+  }
+  remove(i: number) {
+    this.list.splice(i, 1);
+  }
+}
